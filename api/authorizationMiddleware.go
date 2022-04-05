9538c7db52994ca2d00f227b27e5eb0aeffa50e7
rtpa25 <pandaronit25@gmail.com>
2022-04-05 14:03:13 +0530

    completed building the middlewares need to test all of them

--- api/authorizationMiddleware.go
+++ api/authorizationMiddleware.go
@@ -1,9 +1,29 @@
 package api
 
-// import "github.com/gin-gonic/gin"
+import (
+	"log"
 
-// func checkAuthority(server Server) gin.HandlerFunc {
-// 	return func(ctx *gin.Context) {
-// 		server.store.GetUserByEmail()
-// 	}
-// }
+	"github.com/gin-gonic/gin"
+	"github.com/supertokens/supertokens-golang/recipe/emailpassword"
+	"github.com/supertokens/supertokens-golang/recipe/session"
+)
+
+func checkAuthority(server Server) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		sess := session.GetSessionFromRequestContext(ctx)
+		user, err := emailpassword.GetUserByID(sess.GetUserID())
+		if err != nil {
+			log.Fatal(err.Error())
+			ctx.Abort()
+		}
+		userFromSelfDB, err := server.store.GetUserByEmail(ctx, user.Email)
+		if err != nil {
+			log.Fatal(err.Error())
+			ctx.Abort()
+		}
+		if userFromSelfDB.IsAdmin == false {
+			ctx.Abort()
+		}
+		ctx.Next()
+	}
+}
---
+++ api/verifySessionMiddleware.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/supertokens/supertokens-golang/recipe/session"
+	"github.com/supertokens/supertokens-golang/recipe/session/sessmodels"
+)
+
+func (server *Server) verifySession(options *sessmodels.VerifySessionOptions) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session.VerifySession(options, func(rw http.ResponseWriter, r *http.Request) {
+			c.Request = c.Request.WithContext(r.Context())
+			c.Next()
+		})(c.Writer, c.Request)
+		// we call Abort so that the next handler in the chain is not called, unless we call Next explicitly
+		c.Abort()
+	}
+}
